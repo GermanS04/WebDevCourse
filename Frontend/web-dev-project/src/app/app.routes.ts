d696da254aa610350e1ae221447222a6ba4d5170
GermanS04 <german.salasmar@gmail.com>
2025-02-15 19:06:20 -0600

    change of routes path

--- Frontend/web-dev-project/src/app/app.routes.ts
+++ Frontend/web-dev-project/src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './public/home/home.component';
 import { AboutComponent } from './public/about/about.component';
+import { SigninComponent } from './public/signin/signin/signin.component';
 
 export const routes: Routes = [
-    { path: '', component: HomeComponent },
+    { path: '', component: SigninComponent },
+    { path: 'home', component: HomeComponent },
     { path: 'about', component: AboutComponent }
 ];
