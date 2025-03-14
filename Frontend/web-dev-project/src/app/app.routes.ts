c5df8e71593bce881aa4f33a0a0be4be371b8b64
GermanS04 <german.salasmar@gmail.com>
2025-03-14 13:32:49 -0600

    add forgot password in router paths

--- Frontend/web-dev-project/src/app/app.routes.ts
+++ Frontend/web-dev-project/src/app/app.routes.ts
@@ -5,11 +5,13 @@ import { AboutComponent } from './features/about/about.component';
 import { SigninComponent } from './features/login/signin/signin.component';
 import { AuthGuard } from './core/guards/auth.guard';
 import { SignupComponent } from './features/login/signup/signup.component';
+import { ForgotPassComponent } from './features/login/forgot-pass/forgot-pass.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'signin', pathMatch: 'full' },
     { path: 'signin', component: SigninComponent },
     { path: 'signup', component: SignupComponent },
+    { path: 'forgotpassword', component: ForgotPassComponent },
     { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
     { path: 'about', component: AboutComponent, canActivate: [AuthGuard] }
 ];
